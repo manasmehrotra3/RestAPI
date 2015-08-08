35cbfca1e5d8b497164587eddf103858021aa40b
manasmehrotra3 <manasmehrotra3@gmail.com>
2015-08-09 01:40:43 +0530

    Initial Upload

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var http = require('http');
+
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+  if (!req.body) 
+  	return res.sendStatus(400);
+  else {
+  	var username = req.body.username;
+  	var password = req.body.password;
+  	if ((username == 'Anthem') && (password == 'DGVLWP1S')) {
+  		res.send('welcome, ' + req.body.username);
+  	}
+  	else
+  		return res.sendStatus(400)
+  }
+});
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
